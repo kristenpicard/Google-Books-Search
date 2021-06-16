5d5576d24e91333e93043395703dfe52e2fd45fe
Kristen Picard <kristenpicard93@gmail.com>
2021-06-16 13:12:49 -0400

    Working on App.js router

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,19 @@
 import React from "react";
-
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import NoMatch from "./pages/NoMatch";
-
+// import Saved from "./pages/Saved.js";
+// import Search from "./pages/Search.js";
 
 function App() {
   return (
-    
+    <Router>
       <div>
-        
-            <NoMatch />
-         
+        {/* <Route exact path="/" component={Search} />
+        <Route exact path="/search" component={Search} />
+        <Route exact path="/saved" component={Saved} /> */}
+        <Route component={NoMatch} />
       </div>
-    
+    </Router>
   );
 }
 
